41560d5e2d4af891133d415e0ccaa6c0b197bea1
Chris Phelps <c.j.phelps89@gmail.com>
2024-10-22 17:46:00 -0400

    Updated a few things on Portfolio and Resume pages

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,33 @@
+import workoutwizards from "../assets/workout-wizards.png";
+import readmegen from "../assets/readme-gen.png";
 export default function Portfolio() {
   return (
     <div className="flexDiv">
       <h1>Portfolio Page</h1>
-      
+      <div className="container text-center">
+        <div className="row">
+          <div className="col d-flex justify-content-center align-items-center w-100 h-100">
+            <a href="https://workout-wizardz.onrender.com/">
+              <img
+                src={workoutwizards}
+                alt="workout wizards"
+                className="img-fluid w-100 h-100"
+                style={{ objectFix: "cover" }}
+              />
+            </a>
+          </div>
+          <div className="col d-flex justify-content-center align-items-center w-100 h-100">
+            <a href="https://github.com/c-phelps/readme-generator">
+              <img
+                src={readmegen}
+                alt="readme generator"
+                className="img-fluid w-100 h-100"
+                style={{ objectFix: "cover" }}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,24 @@
 export default function Resume() {
+  const downloadResume = () => {
+    const link = document.createElement("a");
+    link.href = "../../public/Christopher_Phelps_Resume.pdf";
+    link.download = "Christopher_Phelps_Resume.docx.pdf";
+    link.click();
+  };
   return (
     <div className="flexDiv">
-      <h1>Resume Page</h1>
-      <p>Resume download goes here</p>
+      <h1>Resume</h1>
+      <div style={{ height: "500px", width: "100%" }}>
+        <iframe
+          src="/Christopher_Phelps_Resume.pdf" 
+          style={{ width: "100%", height: "100%" }}
+          frameBorder="0"
+          title="PDF Viewer"
+        ></iframe>
+      </div>
 
       <div className="d-flex justify-content-center">
-        <button className="btn btn-dark" id="resume">
+        <button onClick={downloadResume} className="btn btn-dark" id="resume">
           Download Resume
         </button>
       </div>
